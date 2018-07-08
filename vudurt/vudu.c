4c5c1820cf55e8686b51e3bd2e8063b023bf44b9
Bob Somers <bob@bobsomers.com>
2018-07-07 18:02:17 -0700

    Initialize/destroy Vulkan instance in shared library ctor/dtor.

--- vudurt/vudu.c
+++ vudurt/vudu.c
@@ -2,14 +2,43 @@
 
 #include <stdio.h>
 
+#include <vulkan/vulkan.h>
+
+static VkInstance vudu_instance;
+
 static void __attribute__((constructor)) vuduCreate() {
-  // TODO: init vulkan, set up static data structures
-  printf("vuduCreate()!\n");
+  VkApplicationInfo ai;
+  ai.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  ai.pNext = NULL;
+  ai.pApplicationName = "vudurt";
+  ai.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  ai.pEngineName = "vudu";
+  ai.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  ai.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo ci;
+  ci.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  ci.pNext = NULL;
+  ci.flags = 0;
+  ci.pApplicationInfo = &ai;
+  ci.enabledLayerCount = 0;
+  ci.ppEnabledLayerNames = NULL;
+  ci.enabledExtensionCount = 0;
+  ci.ppEnabledExtensionNames = NULL;
+
+  VkResult result = vkCreateInstance(&ci, NULL, &vudu_instance);
+  if (result != VK_SUCCESS) {
+    vudu_instance = NULL;
+    return;
+  }
+  printf("Created Vulkan instance!\n");
 }
 
 static void __attribute__((destructor)) vuduDestroy() {
-  // TODO: shutdown vulkan
-  printf("vuduDestroy()!\n");
+  if (vudu_instance != NULL) {
+    printf("Destroying Vulkan instance!\n");
+    vkDestroyInstance(vudu_instance, NULL);
+  }
 }
 
 cudaError_t cudaMalloc(void** devPtr, size_t size) {
